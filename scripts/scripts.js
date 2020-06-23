5ee704c30babfed7531b3c8e229ac9081af2aa8e
Анастасия <A.v.bakina@mail.ru>
2020-06-23 08:29:09 +0600

    Change Mobile menu icon

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,13 +3,21 @@ let modal = document.querySelector('.modal');
 let body = document.querySelector('body');
 let mobileMenuLine = document.querySelector('.menu-line');
 let modalMenu = document.querySelector('.alt-icon');
+let nav = document.querySelector('nav');
+let signUp = document.querySelector('.sign-up');
+let logIn = document.querySelector('.log-in');
+console.log(signUp)
 
 mobileMenu.addEventListener('click', function(){
 modal.classList.toggle('open-menu');
 body.classList.toggle('scroll-lock');
 mobileMenuLine.classList.toggle('hidden');
+nav.classList.toggle('show');
+signUp.classList.toggle('show');
+logIn.classList.toggle('show');
+
 if(mobileMenuLine.classList.contains('hidden')){
 modalMenu.classList.replace('hidden','show');}
 else {modalMenu.classList.replace('show','hidden')}
 });
-console.log(image.setAttribute);
+
